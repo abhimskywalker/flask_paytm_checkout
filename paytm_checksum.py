e2d72737cf6290515432daefe5da2e2ed0c088a3
Atri Patel <64589033+Atri10@users.noreply.github.com>
2021-01-18 22:35:37 +0530

    Added specific UTF-8 Encoding  in Decryption.
    While using Flask (Latest) in Python 3.8 faced issues in decryption which was specifically required utf-8 encoding which is added in required lines in order to generate checksum perfectly.

--- paytm_checksum.py
+++ paytm_checksum.py
@@ -99,8 +99,8 @@ def __encode__(to_encode, iv, key):
     # Pad
     to_encode = __pad__(to_encode)
     # Encrypt
-    c = AES.new(key, AES.MODE_CBC, iv)
-    to_encode = c.encrypt(to_encode)
+    c = AES.new(key.encode("utf-8"), AES.MODE_CBC, iv.encode("utf-8"))
+    to_encode = c.encrypt(to_encode.encode("utf8"))
     # Encode
     to_encode = base64.b64encode(to_encode)
     return to_encode.decode("UTF-8")
@@ -110,7 +110,7 @@ def __decode__(to_decode, iv, key):
     # Decode
     to_decode = base64.b64decode(to_decode)
     # Decrypt
-    c = AES.new(key, AES.MODE_CBC, iv)
+    c = AES.new(key.encode("utf-8"), AES.MODE_CBC, iv.encode("utf-8"))
     to_decode = c.decrypt(to_decode)
     if type(to_decode) == bytes:
         # convert bytes array to str.
